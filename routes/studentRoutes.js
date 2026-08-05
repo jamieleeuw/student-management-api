78806b1932c2c8af969c0a29148a3b20afd4b54e
Jamie Leeuw <mrjtleeuw27@gmail.com>
2026-08-05 20:50:54 +0200

    feat: complete CRUD student API

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -28,4 +28,60 @@ route.get("/:id", (req, res) => {
     res.json(student);
 });
 
+route.post("/",(req,res) =>{
+   
+    if(!req.body.name){
+        return res.status(400).json({
+            message : "Please enter a student name"
+        })
+    }
+   
+    const student = {
+        id : students.length + 1,
+        name: req.body.name
+    }
+    students.push(student)
+    res.status(201).json(student);
+})
+
+
+route.put("/:id",(req,res) =>{
+    const studentId = parseInt(req.params.id);
+
+    const student = students.find(student => student.id === studentId);
+
+    if (!student) {
+        return res.status(404).json({
+            message: "Student not found"
+        });
+    }
+
+    if(!req.body.name){
+        return res.status(400).json({
+            message : "Please enter a student name"
+        })
+    }
+
+    student.name = req.body.name;
+    res.status(200).json(student);
+})
+
+
+route.delete("/:id",(req,res) =>{
+    const studentId = parseInt(req.params.id);
+
+    const student = students.find(student => student.id === studentId);
+
+    if (!student) {
+        return res.status(404).json({
+            message: "Student not found"
+        });
+    }
+
+    const index = students.indexOf(student)
+    students.splice(index,1)
+
+    res.status(200).json(student)
+})
+
 module.exports = route
