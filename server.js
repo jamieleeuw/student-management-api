4b946d8dd724f885f505b73753740b04aeff3fa4
Jamie Leeuw <mrjtleeuw27@gmail.com>
2026-08-10 01:01:28 +0200

    Add JSON body parsing to Express server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json());
 const methodLogger = require('./middlewares/logger')
 const studentRouter = require('./routes/studentRoutes')
 require('dotenv').config()
